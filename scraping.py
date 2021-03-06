432af3fb75d5bb541a46d03683958493831ad9da
Aarani Sivasekaram <aarani.sivasekaram@gmail.com>
2021-03-06 18:39:45 -0500

    Updated scraping code to include all functions

--- scraping.py
+++ scraping.py
@@ -2,15 +2,27 @@
 from splinter import Browser
 from bs4 import BeautifulSoup as soup
 import pandas as pd
+import datetime as dt
 
-# installing chromedriver and installing path
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-driver = webdriver.Chrome(ChromeDriverManager().install())
+# Create scrape all function
+def scrape_all():
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
 
-# Mac users
-executable_path = {'executable_path': '/Users/aaranisivasekaram/.wdm/drivers/chromedriver/mac64/88.0.4324.96/chromedriver'}
-browser = Browser('chrome', **executable_path, headless=False)
+    news_title, news_p = mars_news(browser)
+
+    # Run all scraping functions and store results in a dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_p,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 # Create function for scraping mars news site
 def mars_news(browser):
@@ -90,8 +102,7 @@ def mars_facts():
     # Convert dataframe back to html 
     return df.to_html()
 
-browser.quit()
-
-
-
-
+# Complete script
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
